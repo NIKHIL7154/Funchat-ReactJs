f4eb2494c4ab0609e66016aa7c1c1e5ff21415b0
NIKHIL <nikhilthakurq@gmail.com>
2024-01-27 23:19:34 +0530

    Firebase linked succesfully...

--- src/Components/Chatbox.jsx
+++ src/Components/Chatbox.jsx
@@ -3,30 +3,55 @@ import Message from './Message'
 //import ScrollToBottom, {useScrollToBottom, useSticky } from 'react-scroll-to-bottom';
 import '../Components/Chatbox.css'
 import {app} from '../fbconfig'
-import { getFirestore } from "firebase/firestore";
-import { collection, addDoc, setDoc,doc ,updateDoc} from "firebase/firestore"; 
+import { Timestamp, getFirestore } from "firebase/firestore";
+import { collection, addDoc,orderBy, setDoc,doc ,updateDoc,onSnapshot,getDoc,query, where,getDocs} from "firebase/firestore"; 
 
+var months=["Jan","Feb","Mar","Apr","May","June","july","Aug","Sep","Oct","Nov","Dec"]
+
+function nowww(){
+  
+  let dt = new Date();
+  let hours = dt.getHours() ; // gives the value in 24 hours format
+  let AmOrPm = hours >= 12 ? 'PM' : 'AM';
+  hours = (hours % 12) || 12;
+  let minutes = dt.getMinutes() ;
+  let finalTime = hours + ":" + minutes + " " + AmOrPm+" "+dt.getDate()+" "+months[dt.getMonth()]
+  return finalTime
+}
 const Chatbox = ({datum}) => {
   
-  const [msgs, setmsgs] = useState([<Message deliver={'inn'}/>,<Message/>]);
+  const [msgs, setmsgs] = useState("");
   const msginput = useRef('hello')
-  const scrollers = useRef('')
+  const scrollers = useRef(null)
   const db = getFirestore(app);
+  const nameref = useRef('')
+
+  
 
   useEffect(() => {
-    let jk=0
-    let temparr=[]
-    for(jk;jk<20;jk++){
-      let temp='hh'
-      if(jk%3===0){
-        temp='inn'
-      }
-      console.log(jk+" runs "+temp)
-      temparr.push(<Message deliver={temp} msg={jk+temp}/>)
-    }
-    setmsgs(temparr)
-    jk=6
+    const q = query(collection(db, "users","room/messages"), orderBy("time"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach(async (change) => {
+        let reff=change.doc.data()
+        if (change.type === "added") {
+          let del=""
+          if(reff.Name==="Nikhil"){
+            del="onn"
+          }else{
+            del="inn"
+          }
+          setmsgs(prevArray=>[...prevArray,<Message timeee={reff.nutime} deliver={del} msg={reff.msg}/>])
+          
+        }
+
+      });
+    setTimeout(() => {
+      scrollers.current.scrollTop = scrollers.current.scrollHeight;
+    }, 200);
+});
+
   },[]);
+  
   return (
     <div id='chatboxscroller' className='h-[100%]'>
       <div ref={scrollers} className={`h-[90%] overflow-y-scroll scrole`}>
@@ -36,35 +61,31 @@ const Chatbox = ({datum}) => {
       </div>
       <div className='h-[10%] flex items-center justify-evenly'>
           <input placeholder='Enter message here.....' ref={msginput} type="text" className='px-5 h-[70%] w-[80%]'/>
-          <button onClick={()=>{
+          <button onClick={async ()=>{
             let currentmsg=msginput.current.value
-            setmsgs(prevArray=>[...prevArray,<Message deliver={'onn'} msg={currentmsg}/>])
+            
             msginput.current.value=''
-            setTimeout(async ()=>{
-              scrollers.current.scrollTop = scrollers.current.scrollHeight;
-              try {
-                const frankDocRef = doc(db, "users", "frank");
-                let valu={
-                  
-                }
-                const upda = {
-                  ...valu,
-                  [currentmsg]: currentmsg+"yolo",
-              };
-                await updateDoc(frankDocRef, {
-                  
-                  upda
-                  
-                });
-                console.log("Document written with ID: ", frankDocRef.id);
-              } catch (e) {
-                console.error("Error adding document: ", e);
-              }
-            },200)
+            try {
+              const frankDocRef = collection(db, "users", "room/messages");
+              
+              const final=addDoc(frankDocRef, {
+                Name:nameref.current.value,
+                msg:currentmsg,
+                time:Timestamp.now(),
+                nutime:nowww()
+              });
+              console.log("Document written with ID: ", final.id);
+            } catch (e) {
+              console.error("Error adding document: ", e);
+            }
             
             
           }} className='bg-blue-500 p-3 text-white font-bold rounded-xl'>Send</button>
       </div>
+      <button onClick={()=>{
+        //scrollers.current.scrollTop = scrollers.current.scrollHeight;
+      }}>hello</button>
+      <input type="text" placeholder="enter name" ref={nameref}/>
       
     </div>
     
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import grad from '../assets/gradient.png'
-const Message = ({deliver,msg}) => {
+const Message = ({deliver,msg,timeee}) => {
   if(deliver==='inn'){
   return (
     <div className='flex my-3'>
       <img className='w-8 h-8 rounded-[50%]' src={grad} alt="" />
       <div className='bg-[#ff9c9c] ml-3 mt-3 rounded-b-[10px] min-w-[200px] max-w-[350px] rounded-tr-[10px] p-2'>
         <p>{msg}</p>
-        <p className='text-sm text-right'>12:12AM</p>
+        <p className='text-[10px] text-right'>{timeee}</p>
       </div>
     </div>
   )
@@ -17,7 +17,7 @@ const Message = ({deliver,msg}) => {
           
           <div className='bg-[#ff9c9c] mr-3 mt-3 rounded-b-[10px] min-w-[200px] max-w-[350px] rounded-tl-[10px] p-2'>
             <p>{msg}</p>
-            <p className='text-sm text-right'>12:12AM</p>
+            <p className='text-[10px] text-right'>{timeee}</p>
           </div>
           <img className='w-8 h-8 rounded-[50%]' src={grad} alt="" />
         </div>
